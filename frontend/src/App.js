3fff4e523fbfddbaa3372fa89db3b70fdbff39df
Jorge Luis Bustamante Velasco <jorgeluisbustamantevelasco@MacBook-Pro-de-Jorge.local>
2025-05-05 23:58:13 -0500

    Corrigiendo rutas en App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,10 @@
 // frontend/src/App.js
 
 import React from 'react';
-import { HashRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import RegistroProveedor from './pages/RegistroProveedor';
 import LoginProveedor from './pages/LoginProveedor';
 import FormularioProveedor from './pages/FormularioProveedor';
-import LoginComprador from './pages/LoginComprador';
-import DashboardComprador from './pages/DashboardComprador';
-import ProtectedRoute from './ProtectedRoute';
 
 function App() {
   return (
@@ -16,16 +13,11 @@ function App() {
         <Route path="/" element={<Navigate to="/login" />} />
         <Route path="/register" element={<RegistroProveedor />} />
         <Route path="/login" element={<LoginProveedor />} />
-        <Route path="/formulario" element={
-          <ProtectedRoute>
-            <FormularioProveedor />
-          </ProtectedRoute>
-        } />
-        <Route path="/login-comprador" element={<LoginComprador />} />
-        <Route path="/dashboard" element={<DashboardComprador />} />
+        <Route path="/formulario" element={<FormularioProveedor />} />
       </Routes>
     </Router>
   );
 }
 
 export default App;
+
